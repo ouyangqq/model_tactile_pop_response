7c6aaeab7433e26c3dc53c9ba0b78ac1118a2344
ouyangqq <546163019@qq.com>
2019-07-28 15:38:36 -0700

    Add files via upload

--- Fig.8_Response_to_curve surface.py
+++ Fig.8_Response_to_curve surface.py
@@ -31,8 +31,9 @@ rads=1000/curves #mm
 
 distances=np.arange(-1,4.5,0.5) #mm
 
-
-#-----------run Simulation-for-reponse change as a function of curvature  --------------
+'''
+'delete the '...' above and below if want run the simulation again'
+#-----------simulating reponse change as a function of curvature  --------------
 simulation_res=[]
 eps_buf=[]
 for ch in range(2):  #3 afferent types
@@ -60,8 +61,8 @@ for ch in range(2):  #3 afferent types
 np.save('data/surface_curve_simres.npy',simulation_res)
 np.save('data/surface_curve_eps.npy',eps_buf)
 #-------------------------------------------------------------------
-'''
-'delete the '...' above and below if want run the simulation again'
+
+#-----------simulating reponse change as a function of distance under different curvature--------------
 simulation_res=[]
 eps_buf=[]
 for ch in range(1):  #3 afferent types
